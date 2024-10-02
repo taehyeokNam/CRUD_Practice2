98d8e731c3ff2f47ff28433a8eb44dab9efa671f
taehyeok Nam <navyth91@naver.com>
2024-10-02 11:16:32 +0900

    first commit

---
+++ src/main/java/com/example/crud_practice2/CrudPractice2Application.java
@@ -0,0 +1,13 @@
+package com.example.crud_practice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPractice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crud_practice2/CrudPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_practice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
